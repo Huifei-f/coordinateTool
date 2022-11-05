0c85cdac9a8d295295d9fe4f3d579ad57e1c8d37
YAO <15755842658@163.com>
2022-11-05 22:34:11 +0800

    主线提交Demo2

---
+++ src/main/java/com/tool/coordinate/Demo2.java
@@ -0,0 +1,15 @@
+package com.tool.coordinate;
+
+/**
+ * @author yk
+ * @version 1.0
+ * @description:
+ * @date 2022/11/5 22:33
+ */
+public class Demo2
+{
+
+    public static void main(String[] args) {
+        System.out.println("Demo2");
+    }
+}
